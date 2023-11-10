4b3d852529c6e509d2813480a8da9d22383c897d
Jenres <jenres@res-it.com>
2023-11-10 17:03:07 +0000

    "Added docstrings and logging to taxonomy.py"
    This commit includes the addition of docstrings to the `Taxonomy` class and its methods in the `taxonomy.py` file for better code readability and understanding. It also includes the addition of logging to track the creation of a new taxonomy, the addition of a new breed, and the listing of breeds.

--- repo_prova/taxonomy.py
+++ repo_prova/taxonomy.py
@@ -1,6 +1,17 @@
-class Taxonomy:
+import logging
 
-    def __init__(kingdom:str, clade:str, order:str, family:str, genus:str, species:str):
+logging.basicConfig(level=logging.INFO)
+
+
+class Taxonomy:
+    """
+    A class to represent a biological taxonomy.
+    """
+
+    def __init__(self, kingdom:str, clade:str, order:str, family:str, genus:str, species:str):
+        """
+        Initialize Taxonomy with kingdom, clade, order, family, genus, species.
+        """
         self.kingdom = kingdom
         self.clade = clade
         self.order = order
@@ -9,14 +20,22 @@ class Taxonomy:
         self.species = species
         self.variety = {}
         self.breeds = {}
+        logging.info(f'Taxonomy for {self.genus} {self.species} created.')
     
-    def add_breed(new_breed:str, description:str=None):
+    def add_breed(self, new_breed:str, description:str=None):
+        """
+        Add a new breed to the taxonomy.
+        """
         self.breeds[new_breed] = description
+        logging.info(f'Breed {new_breed} added to {self.genus} {self.species}.')
     
-    def list_breeds():
+    def list_breeds(self):
+        """
+        List all breeds in the taxonomy.
+        """
         if len(self.breeds)==0:
             print(f'{self.genus} {self.species} doesn't have any breed')
         for b, d in self.breeds.items():
             print(b, d)
         return self.breeds
-        
+        logging.info(f'Listing breeds for {self.genus} {self.species}.')
