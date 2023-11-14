5a47350bce30a16a5cd58173fb06e5ff1bbc074c
Yingji Wu <yingji.wu@res-it.com>
2023-11-14 10:15:36 +0100

    fix jenres pull-request

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
 
+package_name = "repo_prova"
+
 setup(
-    name='repo_prova',
-    version='0.1',
+    name=package_name,
+    version='0.0.1',
     packages=find_packages(),
+    package_data={'': ['*.csv']},
     author='Your Name',
     author_email='your.email@example.com',
     description='A short description of the project.',
     long_description=open('README.md').read(),
     install_requires=[],  # List your project dependencies here
-    url='http://example.com/your/project',
+    url='https://github.com/yingji/repo_prova',
+    python_requires='>=3.6.0',
     classifiers=[
         'License :: OSI Approved :: Python Software Foundation License',
         'Programming Language :: Python',
