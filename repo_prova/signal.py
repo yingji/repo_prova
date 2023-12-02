ca493fb29f5c28cf96346fa823d50643d96c630b
yingji <yingjiwu90@gmail.com>
2023-12-02 19:02:10 +0100

    add signal class

---
+++ repo_prova/signal.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 17:35:20 2023
+
+@author: wuy
+"""
+
+import numpy as np
+import scipy.signal as spsgn
+import scipy.stats as spstats
+import pandas as pd
+from typing import List
+# import matplotlib.pyplot as plt
+
+
+class Signal():
+    """
+    a class for signal pricessing
+    """
+    
+    def __init__(self, data: np.ndarray, 
+                 t: np.array = None, fs: int = None,
+                 sig_names: List[str] = None
+                 ):
+        """
+        
+        Parameters
+        ----------
+        data : np.ndarray
+            signal data.
+        t : np.array, optional
+            timestamp. The default is None.
+        fs : int, optional
+            sample rate. The default is None.
+        sig_names: list of str, optional
+            signal names
+        """
+        if t is None and fs is None:
+            raise IOError("Either t or fs must given")
+        
+        if t is not None:
+            self.t = t
+        else:
+            self.t = np.arange(len(data))/fs
+        if fs is not None:
+            self.fs = fs
+        else:
+            self.fs = int(1/spstats.mode(np.diff(t))[0])
+        self.data = data
+        if sig_names is None:
+            self.sig_names = [f'ch{i}' for i in range(self.data.shape[1])]
+        else:
+            if len(sig_names) != self.data.shape[1]:
+                raise ValueError(
+                    "Mismatching size: signal_names must have same number of the given signals"
+                    )
+            self.sig_names = sig_names
+    
+    def band_filter(self, freq_range, order=3):
+        """Zero-phase filter"""
+        
+        b_hp, a_hp = spsgn.butter(order, freq_range[0] * 2 / self.fs, btype='high')
+        b_lp, a_lp = spsgn.butter(order, freq_range[1] * 2 / self.fs, btype='low')
+        filt_data = spsgn.filtfilt(
+            b_hp, a_hp, 
+            spsgn.filtfilt(b_lp, a_lp, self.data, axis=0), 
+            axis=0
+        )
+        return Signal(data=filt_data, t=self.t, fs=self.fs, sig_names=self.sig_names)
+    
+    def power_spectral_density(self):
+        nblock = 1024
+        overlap = 128
+        sig_pxxf = []
+        win = spsgn.hanning(nblock, True)
+        for i in range(self.data.shape[1]):
+            f, pxxf = spsgn.welch(
+                self.data[:,i], self.fs, 
+                window=win, noverlap=overlap, nfft=nblock, 
+                detrend='constant', return_onesided=True
+                )
+            sig_pxxf.append(pxxf[..., None])
+        # plt.semilogy(f, pxxf, '-o')
+        # plt.plot(f, pxxf2, '-o')
+        return f, np.hstack(sig_pxxf)
+    
+    def stats(self):
+        df = pd.DataFrame(self.data, columns=self.sig_names)
+        return df.describe()
---
+++ tests/test_signal.py
@@ -0,0 +1,40 @@
+import os
+import wfdb
+import numpy as np
+from repo_prova.signal import Signal
+import unittest
+
+current_path = os.path.abspath(__file__)
+
+class TestSignal(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(IOError): # missing fs and t
+            Signal(data = np.zeros(6))
+        with self.assertRaises(ValueError): # mismatched signal names
+            Signal(data = np.hstack((np.arange(9)[..., None], np.ones(9)[..., None] )),
+                   t=np.arange(9),
+                   sig_names=['ciao'])
+
+
+class TestNSTSignal(unittest.TestCase):
+
+    def setUp(self):
+        record = wfdb.rdrecord('tests/nstdb/118e_6')
+        # wfdb.plot_wfdb(record=record, title='Example signals')
+        self.sig = Signal(data = record.p_signal, fs= record.fs)
+    
+    def test_filter(self):
+        sig_filt = self.sig.band_filter([0.5, 40])
+        np.testing.assert_almost_equal(
+            np.average(sig_filt.data, axis=0),
+            np.zeros(2),
+            decimal=3
+            )
+    
+    def test_power_spectral_density(self):
+        f, pxx = self.sig.power_spectral_density()
+        for i in range(pxx.shape[1]):
+            self.assertTrue(0.05<f[np.argmax(pxx[:,i])]<100)
+    
+if __name__ == '__main__':
+    unittest.main()
